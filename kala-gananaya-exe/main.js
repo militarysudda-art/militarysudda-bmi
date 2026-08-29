a2eb0c210588b93d436d379641b1d32d5d96d489
militarysudda-art <militarysudda@gmail.com>
2026-08-29 07:52:21 +0530

    Add Electron main process

---
+++ kala-gananaya-exe/main.js
@@ -0,0 +1,3 @@
+const {app,BrowserWindow,ipcMain}=require('electron');const path=require('path');
+function createWindow(){const win=new BrowserWindow({width:1200,height:850,minWidth:900,minHeight:650,webPreferences:{preload:path.join(__dirname,'preload.js'),contextIsolation:true,nodeIntegration:false}});win.loadFile('index.html');}
+app.whenReady().then(()=>{createWindow();ipcMain.handle('save-pdf',async(event)=>{const win=BrowserWindow.fromWebContents(event.sender);return await win.webContents.printToPDF({printBackground:true,pageSize:'A4',margins:{marginType:'default'}})});app.on('activate',()=>{if(BrowserWindow.getAllWindows().length===0)createWindow()})});app.on('window-all-closed',()=>{if(process.platform!=='darwin')app.quit()});
